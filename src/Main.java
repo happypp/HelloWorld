de2225b49dc709c231579f9b90eb9a4fd2731b75
pp <957655440@qq.com>
2015-10-04 13:35:30 +0800

    add 方法的添加

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!测试");
+        add(12,13);
+    }
+
+
+    public void add(int a, int b){
+        System.out.println(a+b);
     }
 }
